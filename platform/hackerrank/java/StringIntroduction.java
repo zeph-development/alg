3c1f387650f410c9ace258a59c1b0c0a624fb256
pancudaniel7 <pancudaniel7@gmail.com>
2021-11-07 00:34:50 +0200

    SubString order

--- platform/hackerrank/java/StringIntroduction.java
+++ platform/hackerrank/java/StringIntroduction.java
@@ -25,8 +25,27 @@ public class StringIntroduction {
         System.out.println(S.substring(start, end));
     }
 
+    private static String subStringOrder(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        smallest = largest = s.substring(0, k);
+
+        for (int i=1; i<s.length()-k+1; i++) {
+            String substr = s.substring(i, i+k);
+            if (smallest.compareTo(substr) > 0)
+                smallest = substr;
+            if (largest.compareTo(substr) < 0)
+                largest = substr;
+        }
+
+        return smallest + "\n" + largest;
+    }
+
     public static void main(String... args) {
         // compareStrings();
-        subString();
+        // subString();
+
+        System.out.println(subStringOrder("welcomeToJava", 3));
+    
     }
 }
