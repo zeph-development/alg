3c07166add890255fdec544a83aaffd507eb2576
pancudaniel7 <pancudaniel7@gmail.com>
2021-12-04 16:31:23 +0200

    Is prime Hackerrank

---
+++ platform/hackerrank/java/Primality.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Primality {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        boolean isPrime = new BigInteger(n).isProbablePrime(1);
+        if (isPrime) {
+            System.out.print("prime");
+        }
+        bufferedReader.close();
+    }
+}
