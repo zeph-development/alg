180cb46bbd526d126cc2b59891d7ad9d46e64a90
Daniel <pancudaniel7@gmail.com>
2023-01-12 15:35:39 +0200

    Implemented Observer pattern

---
+++ patterns/Observer.kt
@@ -0,0 +1,64 @@
+package com.learning.kotlinlearning
+
+fun main() {
+    val publisher: Publisher = Publisher()
+
+    publisher.subscribe(CarListener())
+    publisher.subscribe(HouseListener())
+    publisher.subscribe(GardenListener())
+
+    publisher.notify("action1")
+}
+
+class Observer {
+    fun main() {
+        val publisher: Publisher = Publisher()
+
+        publisher.subscribe(CarListener())
+        publisher.subscribe(HouseListener())
+        publisher.subscribe(GardenListener())
+
+        publisher.notify("action1")
+    }
+}
+
+class Publisher {
+    private val listeners: LinkedHashSet<Listener> = LinkedHashSet<Listener>()
+
+    fun subscribe(listener: Listener) {
+        listeners.add(listener)
+    }
+
+    fun unsubscribe(listener: Listener){
+        listeners.remove(listener)
+    }
+
+    fun notify(action: String) {
+        listeners.forEach {
+            it.update(action);
+        }
+    }
+
+}
+
+interface Listener {
+    fun update(action: String)
+}
+
+class GardenListener : Listener {
+    override fun update(action: String) {
+        println("Garden listener $action\n")
+    }
+}
+
+class HouseListener : Listener {
+    override fun update(action: String) {
+        println("House listener $action")
+    }
+}
+
+class CarListener : Listener {
+    override fun update(action: String) {
+        println("Car listener $action")
+    }
+}
