9c8086cfbe31e122fb9c13a01cc7afba959866ce
Daniel <pancudaniel7@gmail.com>
2023-07-28 20:08:43 +0300

    Graph incidence matrix example

--- graph/src/main/java/com/example/App.java
+++ graph/src/main/java/com/example/App.java
@@ -105,29 +105,70 @@ class DetectCycle {
     }
 }
 
+class GraphIncidenceMatrixExample {
+    private int[][] incidenceMatrix;
+    private int numVertices;
+    private int numEdges;
+
+    public GraphIncidenceMatrixExample(int numVertices, int numEdges) {
+        this.numVertices = numVertices;
+        this.numEdges = numEdges;
+        incidenceMatrix = new int[numVertices][numEdges];
+    }
+
+    public void addEdge(int edgeIndex, int vertex1, int vertex2) {
+        // Set the incidence matrix cells for the edge
+        incidenceMatrix[vertex1][edgeIndex] = 1;
+        incidenceMatrix[vertex2][edgeIndex] = 1;
+    }
+
+    public void printIncidenceMatrix() {
+        System.out.println("Incidence Matrix:");
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numEdges; j++) {
+                System.out.print(incidenceMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
 public class App {
 
     public static void main(String[] args) {
-        Map<String, String[]> graph = new HashMap<>();
-        graph.put("b", new String[]{"d", "a"});
-        graph.put("a", new String[]{"b", "c"});
-        graph.put("c", new String[]{"e", "a"});
-        graph.put("d", new String[]{"f", "b"});
-        graph.put("e", new String[]{"c"});
-        graph.put("f", new String[]{"d"});
+//        Map<String, String[]> graph = new HashMap<>();
+//        graph.put("b", new String[]{"d", "a"});
+//        graph.put("a", new String[]{"b", "c"});
+//        graph.put("c", new String[]{"e", "a"});
+//        graph.put("d", new String[]{"f", "b"});
+//        graph.put("e", new String[]{"c"});
+//        graph.put("f", new String[]{"d"});
 
 //        new DepthFirstTraversal(graph).dft("a");
 //        new BreadthFirstTraversal(graph).bft("a");
 
-        Map<String, String[]> unidirectionalGraph = new HashMap<>();
-        unidirectionalGraph.put("a", new String[]{"b"});
-        unidirectionalGraph.put("b", new String[]{"c"});
-        unidirectionalGraph.put("c", new String[]{"d"});
-        unidirectionalGraph.put("d", new String[]{"e"});
-        unidirectionalGraph.put("e", new String[]{"b"});
+//        Map<String, String[]> unidirectionalGraph = new HashMap<>();
+//        unidirectionalGraph.put("a", new String[]{"b"});
+//        unidirectionalGraph.put("b", new String[]{"c"});
+//        unidirectionalGraph.put("c", new String[]{"d"});
+//        unidirectionalGraph.put("d", new String[]{"e"});
+//        unidirectionalGraph.put("e", new String[]{"b"});
+//
+//        boolean isCycle = new DetectCycle(unidirectionalGraph).detectCycle();
+//        System.out.println(isCycle);
 
-        boolean isCycle = new DetectCycle(unidirectionalGraph).detectCycle();
-        System.out.println(isCycle);
+        int numVertices = 4;
+        int numEdges = 5;
+        GraphIncidenceMatrixExample graph = new GraphIncidenceMatrixExample(numVertices, numEdges);
+
+        graph.addEdge(0, 0, 1);
+        graph.addEdge(1, 0, 2);
+        graph.addEdge(2, 1, 2);
+        graph.addEdge(3, 2, 3);
+        graph.addEdge(4, 0, 3);
+
+        graph.printIncidenceMatrix();
     }
 
 }
